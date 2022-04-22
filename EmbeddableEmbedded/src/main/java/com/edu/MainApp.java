f947ebb19d3cc7eaa8be37c923b04b6ec3bcdb84
GOMATHI KAVI <GOMATHI KAVI@DESKTOP-AB6DJ25>
2022-04-21 21:38:01 -0700

    GitHub

---
+++ EmbeddableEmbedded/src/main/java/com/edu/MainApp.java
@@ -0,0 +1,29 @@
+package com.edu;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Configuration con= new Configuration().configure().addAnnotatedClass(Employee.class);
+		 ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+		 SessionFactory factory= con.buildSessionFactory(reg);
+		 Session session= factory.openSession();
+		 Transaction t= session.beginTransaction();
+		 
+		
+		
+		 Address ad=new Address("Trichy", "TamilNadu");
+		 
+		 Employee em=new Employee(20, "Bhavadharani",ad);
+		 session.save(em);
+		 t.commit();
+		 session.close();
+	}
+
+}
